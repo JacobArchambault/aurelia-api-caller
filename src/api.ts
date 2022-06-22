0e79790b8ce077d58c47c611d26550beb5c8f12b
Jacob Archambault <Jacob.Archambault@venminder.com>
2022-06-22 13:27:32 -0400

    Make base url for quote api Https instead of http

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import { autoinject } from 'aurelia-framework';
 import { HttpClient } from 'aurelia-fetch-client';
 
-const baseUrl = "http://ron-swanson-quotes.herokuapp.com/v2";
+const baseUrl = "https://ron-swanson-quotes.herokuapp.com/v2";
 
 @autoinject
 export class ApiWrapper {
